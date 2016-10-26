632b8f754c935bf203261d4090ade1b05f05be4d
Erika Mendes <erika.mendes@FEDUC.BR>
2016-10-26 14:22:10 -0200

    exercico 1

---
+++ js/exercicio-1.js
@@ -0,0 +1,128 @@
+alert("Vamos almoçar?")
+
+var numerOne = 1;
+var numberTwo = 2;
+
+numberOne + numberTwo
+
+numberOne / numberTwo
+
+numberOne - numbertwo
+
+
+/* Exercicio 2 */
+3 > 5
+false
+5 > 0
+true
+5 > 5
+false
+0 >= 1
+false
+1 > -10
+true
+
+    /* Exercicio 3 */
+
+    (10 + 40) >= (50 - 10)
+true
+1 > -10
+true
+    (3 > 5) && (5 > 6)
+false
+    (5 < 0) && (0 == 0)
+false
+    (5 != 4) || (4 > 4)
+true
+
+/* Exercicio 4 */
+var erikaMendes = 1
+var elaineValverde = 3
+if (erikaMendes > elaineValverde) {
+    console.log("é maior");
+}
+else {
+    console.log("é menor");
+};
+
+/*resposta 4*/
+var erikaMendes = 1
+var elaineValverde = 3
+if (erikaMendes > elaineValverde) {
+    console.log("é maior");
+}
+else {
+    console.log("é menor");
+};
+é menor
+
+/* Exercicio 5 */
+var numberTwo
+var numberOne
+if (2 > 1) {
+    console.log("é maior");
+}
+else {
+    console.log("é menor");
+};
+/*Resposta*/
+var numberTwo = 6
+var numberOne = 4
+if (numberTwo > numberOne) {
+    console.log("é maior");
+}
+else {
+    console.log("é menor");
+};
+é maior
+
+
+var numberTwo = 10
+var numberOne = 10
+if (numberTwo == numberOne) {
+    console.log("valor igual");
+}
+else {
+    console.log("é menor");
+};
+/* resposta */
+var numberTwo = 10
+var numberOne = 10
+if (numberTwo == numberOne) {
+    console.log("valor igual");
+}
+else {
+    console.log("é menor");
+};
+valor igual
+
+/*exercicio 6*/
+
+var numberOne = prompt("Coloque seu numero")
+var numberTwo = prompt("Coloque outro numero")
+alert(numberOne * numberTwo)
+
+/*Exercicio 7*/
+
+var idade = prompt("Qual a sua idade?")
+if (idade >= 18) { alert("Parabens, voce ja pode dirigir!!"); }
+
+else { alert("Você ainda não pode dirigir!!"); }
+
+/*Exercicio 8 */ 
+var nome = prompt("Qual seu nome? Preenchimento obrigatorio*") 
+var sexo = prompt("Sexo")
+if  (sexo == "feminino") {alert("Bem-Vinda, " + nome );}
+else if (sexo == "masculino") {alert("Bem-Vindo, " + nome );}
+else {alert("Olá, "+ nome);}
+
+
+/* Exercicio 9*/
+var tempo = prompt("Que horas são?")
+if ((tempo == 0 )||(tempo <= 19)) {alert ("Dia");}
+else if ((tempo == 20)||(tempo<=23 )){alert ("Noite");}
+else {alert("Horario incorreto");}
+
+
+
+
